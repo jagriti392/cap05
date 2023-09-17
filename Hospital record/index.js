c12c44b78d5765e256f88ada948e4b5d29614d79
jagriti392 <jagritiupadhyay95@gmail.com>
2023-09-17 19:46:27 +0530

    added Hospital record

--- Hospital record/index.js
+++ Hospital record/index.js
@@ -1,42 +1,66 @@
-// fill in javascript code here
-yFrom = addEventListener("submit", function(e){
-    e.preventDefault();
-     let name = document.getElementById("name").value;
-     let employeeID = document.getElementById("employeeID").value;
-     let department = document.getElementById("department").value;
-     let expereince = document.getElementById("exp").value;
-     let email = document.getElementById("email").value;
-     let mobile = document.getElementById("mbl").value;
-     let role;
-     if(expereince > 5)
-     {
-         role = "Senior";
-     }
-     else if( expereince >=2 && expereince <= 5)
-     {
-         role = "Junior";
-     }
-     else
-     {
-         role  = "Fresher";
-     }
-     let tbody = document.querySelector("tbody");
-     let row = document.createElement("tr");
-     let data = [name, employeeID, department, expereince, email, mobile, role]
-     data.forEach((ele)=>{
-         let td = document.createElement("td");
-         td.innerText = ele
-         row.appendChild(td);
-     })
-     // Delete function
-     let deleteTd = document.createElement("td");
-     let deleteBtn = document.createElement("button");
-     deleteBtn.innerText="Delete";
-     deleteBtn.addEventListener("click",function(){
-         tbody.removeChild(row);
-     })
-     deleteTd.appendChild(deleteBtn);
-     row.appendChild(deleteTd);
-     tbody.appendChild(row);
+let name=document.getElementById("name")
+let doctor_id=document.getElementById("docID")
+let specialization=document.getElementById("dept")
+let experience=document.getElementById("exp")
+let email=document.getElementById("email")
+let mobile=document.getElementById("mbl")
+let form=document.querySelector("form");
+let tbody=document.querySelector("tbody")
+let arr=[] 
+form.addEventListener("submit",function(el){
+  el.preventDefault()
+  let obj={
+    name:name.value,
+    doctor_id:doctor_id.value,
+    specialization:specialization.value,
+    experience:experience.value,
+    email:email.value,
+    mobile:mobile.value,
+
+
+  }
+
+  console.log(obj)
+  let role=""
+  if(+experience.value<=1){
+    role="Trainee"
+  }else if(+experience.value>=2 && +experience.value<=5){
+    role="Junior"
+  }else if(+experience.value > 5){
+    role="Senior"
+  }
+
+  obj.role = role
+  arr.push(obj)
+  
+  
+  arr.forEach((element,index)=>{
+    let tr=document.createElement("tr");
+    let td1=document.createElement("td");
+    let td2=document.createElement("td")
+    let td3=document.createElement("td")
+    let td4=document.createElement("td")
+    let td5=document.createElement("td")
+    let td6=document.createElement("td")
+    let td7=document.createElement("td")
+    let td8=document.createElement("td")
+
+    td1.innerText=element.name;
+    td2.innerText=element.doctor_id;
+    td3.innerText=element.specialization;
+    td4.innerText=element.experience;
+    td5.innerText=element.email;
+    td6.innerText=element.mobile;
+    td7.innerText=element.role;
+    td8.innerText="delete"
+
+    tr.append(td1,td2,td3,td4,td5,td6,td7,td8)
+    tbody.append(tr)
+    td8.addEventListener("click",function(){
+      tr.remove()
+    })
+   
   })
 
+  arr=[];
+})
